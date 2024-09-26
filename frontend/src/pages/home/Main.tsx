87d07e9be5b6561c3d9365b1de466e717590dd0d
ChrisAnhThien <nathien2310@gmail.com>
2024-09-25 21:55:43 -0400

    First commit

---
+++ frontend/src/pages/home/Main.tsx
@@ -0,0 +1,12 @@
+//This main page is used to test on implementing main page including dashboard, team, project, etc
+import React from 'react';
+
+const Main: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default Main;
