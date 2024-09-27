934659187fe19beb1ebbf10b47cc841159042254
Juan-Carlos Sreng-Flores <srengfloresj@hotmail.com>
2024-09-27 15:41:07 -0400

    added more comments and made some fixes

--- backend/middleware/auth_session.go
+++ backend/middleware/auth_session.go
@@ -11,27 +11,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// This function takes care of configuring the secret key and store for the
-// session middleware for the server.
-// It returns a handler function that is used as a middleware
+// Function constructs a middleware that configures session for receiving requests
 func SessionMiddleware() gin.HandlerFunc {
+	// get the secret key for the cookie store
 	sessionSecretKey, exists := os.LookupEnv(("SESSION_SECRET_KEY"))
 	if !exists {
 		// print error and exit
 		log.Fatal("SESSION_SECRET_KEY environment variable is not defined please create a secret key.")
 	}
+	// create a cookie store
 	store := cookie.NewStore([]byte(sessionSecretKey))
-
+	// set the configurations for the session store
+	store.Options(config.SessionConfig)
 	// create the auth session middleware associated with this cookie store
 	return sessions.Sessions("session", store)
 }
 
+// Function constructs a middleware that requires a user to be authenticated
 func AuthenticationMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		session := sessions.Default(c)
 		username := session.Get(config.SessionFields.Username)
 		if username == nil {
-			c.Status(http.StatusUnauthorized)
+			c.Redirect(http.StatusFound, "/login")
 			c.Abort()
 			return
 		}
