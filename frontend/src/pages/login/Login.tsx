9450d94f88c585d1890f2b01888e7bb28b42d66b
Calvin Lee <calvintp4414@gmail.com>
2024-09-28 22:14:11 -0400

    Login v.2

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -62,7 +62,7 @@ export default function LoginPage() {
                             <path d="M4.462 19.462c.42-.419.753-.89 1-1.395.453.214.902.435 1.347.662a6.742 6.742 0 0 1-1.286 1.794.75.75 0 0 1-1.06-1.06Z" />
                         </svg>
                         <h2 className="mt-6 text-center text-3xl font-bold text-gray-900">
-                            Sign in to your account
+                            Log in to your account
                         </h2>
                         <p className="mt-2 text-center text-sm text-gray-600">
                             Or Sign up for a new account as a {" "}
@@ -81,8 +81,7 @@ export default function LoginPage() {
                             </Link>
                         </p>
                     </div>
-                    <form className="mt-8 space-y-6" action="#" method="POST">
-                        <input type="hidden" name="remember" defaultValue="true" />
+                    <form className="mt-8 space-y-6" action="/api/login" method="POST">
                         <div className="-space-y-px rounded-md shadow-sm">
                             <div>
                                 <label htmlFor="email" className="sr-only">
@@ -93,11 +92,12 @@ export default function LoginPage() {
                                     name="email"
                                     type="email"
                                     required
-                                    className="relative block w-full appearance-none rounded-t-md border border-gray-300 px-3 py-2 placeholder:text-gray-400 focus:border-[#ca3448] focus:outline-none focus:ring-[#ca3448] sm:text-sm"
+                                    className="relative block w-full appearance-none rounded-lg border border-gray-300 px-3 py-2 placeholder:text-gray-400 focus:border-[#ca3448] focus:outline-none focus:ring-[#ca3448] sm:text-sm"
                                     placeholder="Email address"
                                 />
                             </div>
                             <div>
+                                <br></br>
                                 <label htmlFor="password" className="sr-only">
                                     Password
                                 </label>
@@ -106,7 +106,7 @@ export default function LoginPage() {
                                     name="password"
                                     type="password"
                                     required
-                                    className="relative block w-full appearance-none rounded-b-md border border-gray-300 px-3 py-2 placeholder:text-gray-400 focus:border-[#ca3448] focus:outline-none focus:ring-[#ca3448] sm:text-sm"
+                                    className="relative block w-full appearance-none rounded-lg border border-gray-300 px-3 py-2 placeholder:text-gray-400 focus:border-[#ca3448] focus:outline-none focus:ring-[#ca3448] sm:text-sm"
                                     placeholder="Password"
                                 />
                             </div>
@@ -117,6 +117,7 @@ export default function LoginPage() {
                                     id="remember-me"
                                     name="remember-me"
                                     type="checkbox"
+                                    value="true"
                                     className="h-4 w-4 rounded border-gray-300 text-[#ca3448] focus:ring-[#ca3448]"
                                 />
                                 <label
@@ -140,7 +141,7 @@ export default function LoginPage() {
                                 type="submit"
                                 className="flex w-full justify-center rounded-md border border-transparent bg-[#ca3448] py-2 px-4 text-sm font-semibold text-white hover:bg-[#E9D3D7] hover:text-[#2c2c2c] focus:outline-none focus:ring-2 focus:ring-[#ca3448] focus:ring-offset-2"
                             >
-                                Sign in
+                                Log in
                             </button>
                         </div>
                     </form>
