fc01924b998915b88d4ecbbbcdb37c64a5dae6b8
Daniel Lam <lam.daniel.123@hotmail.com>
2024-10-02 02:14:03 -0400

    Implement Sign Up for new Students + create user to DB

--- backend/controllers/authentication.go
+++ backend/controllers/authentication.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"backend/config"
 	"backend/database"
+	"backend/models"
+	"backend/util"
 	"context"
 	"fmt"
 	"net/http"
@@ -13,16 +15,9 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-// login body data definiton
-type Login struct {
-	Email      string `form:"email" json:"email" binding:"required"`
-	Password   string `form:"password" json:"password" binding:"required"`
-	RememberMe bool   `form:"remember-me" json:"remember-me"`
-}
-
 // login controller supports two encoding: json and x-www-form-urlencoded
 func LoginController(c *gin.Context) {
-	var body Login
+	var body models.Login
 	invalidFormatResponse := gin.H{"error": "invalid format"}
 	switch c.ContentType() {
 	case "application/json":
@@ -54,16 +49,8 @@ func LoginController(c *gin.Context) {
 	}
 
 	var ok bool
-	password := result["password"].(string)
-	if body.Password == password {
-		ok = true
-	}
-	// ok := util.CompareHashAndPassword(body.Password, password)
-	// fmt.Println(util.HashPassword(body.Password))
-	// if body.Email != "user@hotmail.com" || body.Password != "password" {
-	// 	c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid credentials or format"})
-	// 	return
-	// }
+	hashedPassword := result["password"].(string)
+	ok = util.CompareHashAndPassword(hashedPassword, body.Password)
 
 	if !ok {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid credentials or format"})
@@ -82,40 +69,49 @@ func LoginController(c *gin.Context) {
 	c.Redirect(http.StatusFound, "/dashboard")
 }
 
-// Signup body data definition
-type SignUp struct {
-	Username string `form:"username" json:"username" binding:"required"`
-	Email    string `form:"email" json:"email" binding:"required"`
-	Password string `form:"password" json:"password" binding:"required"`
-	Role     string `form:"role" json:"role" binding:"required"`
-}
-
 // This function takes care of signing up a user to the database
 func SignUpController(c *gin.Context) {
-	var body SignUp
+	var user models.User
+
+	// Converts data from form to the values needed for User struct
 	invalidFormatResponse := gin.H{"error": "invalid format"}
 	switch c.ContentType() {
 	case "application/json":
-		if err := c.ShouldBindBodyWithJSON(&body); err != nil {
+		if err := c.ShouldBindBodyWithJSON(&user); err != nil {
 			c.JSON(http.StatusUnauthorized, invalidFormatResponse)
 			return
 		}
 
 	case "application/x-www-form-urlencoded":
-		if err := c.ShouldBind(&body); err != nil {
+		if err := c.ShouldBind(&user); err != nil {
 			c.JSON(http.StatusUnauthorized, invalidFormatResponse)
 			return
 		}
 	}
 
 	// TODO: CREATE USER IN DATABASE AND HASH PASSWORD
+	if hashedPassword, err := util.HashPassword(user.Password); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error when hashing password!"})
+		return
+	} else {
+		user.Password = hashedPassword
+	}
 
-	// hashedPassword, err := util.HashPassword(body.Password)
+	// Insert the new user
+	collection := database.GetInstance().Database("RateMyPeersDB").Collection("Users")
+
+	// TODO: ADD CODE
+
+	_, err := collection.InsertOne(context.TODO(), user)
+	if err != nil {
+		c.JSON(http.StatusConflict, gin.H{"error": "Error when inserting new user to DB!"})
+		return
+	}
 
 	// once all data is put in db, create session for user
 	session := sessions.Default(c)
-	session.Set(config.SessionFields.Email, body.Email)
-	session.Set(config.SessionFields.Role, body.Role)
+	session.Set(config.SessionFields.Email, user.Email)
+	session.Set(config.SessionFields.Role, user.Role)
 
 	if err := session.Save(); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Session could not be saved, try logging in instead"})
