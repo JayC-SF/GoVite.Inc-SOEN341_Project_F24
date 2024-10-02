8dee0effaf978e78f0f7158fb2ca4957e93b3fb9
ChrisAnhThien <nathien2310@gmail.com>
2024-10-02 15:12:42 -0400

    Commit dashboard

--- frontend/src/pages/home/SidebarOptions.tsx
+++ frontend/src/pages/home/SidebarOptions.tsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 export const SidebarOptions = () => {
   return (
     <div>
-        <nav className="flex min-w-[240px] flex-col gap-1 p-2 font-sans text-base font-normal text-blue-gray-700">
+        <nav className="flex min-w-[200] flex-col gap-1 p-2 font-sans text-base font-normal text-blue-gray-700">
     <div role="button"
       className="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-stone-300 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900">
       <div className="grid mr-4 place-items-center">
