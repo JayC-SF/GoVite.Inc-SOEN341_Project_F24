42dfbd236145563b5d7c4209cdb93090a2144267
Juan-Carlos Sreng-Flores <srengfloresj@hotmail.com>
2024-10-15 14:35:48 -0400

    renamed sidebar page to sidebarpage template

--- frontend/src/templates/SidebarPageTemplate.tsx
+++ frontend/src/templates/SidebarPageTemplate.tsx
@@ -1,10 +1,10 @@
 import { ReactNode } from 'react'
 import { Sidebar } from '../modules/Sidebar'
-interface SidebarPageProps { 
+interface SidebarPageTemplateProps { 
     hidden: boolean,
     children?: ReactNode 
 }
-export default function SidebarPage({ hidden, children }: SidebarPageProps) {
+export default function SidebarPageTemplate({ hidden, children }: SidebarPageTemplateProps) {
     
     return !hidden && (
         <div className='grid gap-4 p-4 grid-cols-[200px,_1fr] text-stone-950 bg-primary-red'>
