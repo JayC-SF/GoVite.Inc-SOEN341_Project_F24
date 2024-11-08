a95b83250781f1aa1e3db1c483610cb0438cc73e
Juan-Carlos Sreng-Flores <srengfloresj@hotmail.com>
2024-11-08 17:43:41 -0500

    add new links, and pages for the course page

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -4,9 +4,11 @@ import TeacherSignUp from "./pages/sign-up/TeacherSignUp";
 import StudentSignUp from "./pages/sign-up/StudentSignUp";
 import Login from "./pages/login/Login";
 import Main from "./pages/main/MainPage";
-import CoursePage from "./pages/CoursePage";
 import GroupPage from "./pages/home/GroupPage";
 import RatePage from "./pages/RatePage";
+import { CourseDetailsPage } from "./pages/course/CourseDetailsPage";
+import { CourseSummaryPage } from "./pages/course/CourseSummaryPage";
+import CoursePage from "./pages/course/CoursePage";
 
 export const AppRoutes = createBrowserRouter([
   {
@@ -32,6 +34,14 @@ export const AppRoutes = createBrowserRouter([
   {
     path: "/courses/:courseid",
     element: <CoursePage/>
+  },
+  {
+    path: "/courses/:courseid/details",
+    element: <CourseDetailsPage/>
+  },
+  {
+    path: "/courses/:courseid/summary",
+    element: <CourseSummaryPage/>
   },
   {
     path: "/groups/:groupid",
