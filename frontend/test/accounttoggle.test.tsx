4d10ba47d9594c406a7ac0c5b7c86a83c86a0129
Chris Anh Thien Nguyen <56431971+chrisanhthien@users.noreply.github.com>
2024-11-13 20:18:56 -0500

    add unit test for Dashboard page (#186)

---
+++ frontend/test/accounttoggle.test.tsx
@@ -0,0 +1,12 @@
+import { describe, it, expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import {AccountToggle} from '../src/modules/AccountToggle';
+import React, { isValidElement } from 'react';
+import '@testing-library/jest-dom';
+
+//test sidebar logo
+test('Sidebar logo', ()=>{
+  render(<AccountToggle/>)
+  const imgElement = screen.getByRole('img')
+  expect(imgElement).toHaveClass('size-8');
+})
