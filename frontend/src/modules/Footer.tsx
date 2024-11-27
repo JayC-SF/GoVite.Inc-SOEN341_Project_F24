35b91d91f02c12d0639561d55299cebf0657048a
Calvin Lee <calvintp4414@gmail.com>
2024-11-26 19:19:08 -0500

    Footer info update

--- frontend/src/modules/Footer.tsx
+++ frontend/src/modules/Footer.tsx
@@ -2,11 +2,17 @@
 const Footer = () => {
   return (
     <div>
-    <footer className="bg-gray-800 text-white py-5 mt-10 w-full">
-      <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} GoVite Inc. All rights reserved.</p>
-      </div>
-    </footer>
+      <footer className="bg-gray-800 text-white py-5 mt-10 w-full">
+        <div className="container mx-auto text-center">
+          <p className="text-sm text-gray-400 mt-2 mb-2">
+            Need more help? Contact us at{" "}
+            <a href="tel:514-848-2424" className="text-[#ca3448] hover:text-[#E9D3D7]">
+              514-848-2424
+            </a>
+          </p>
+          <p>&copy; {new Date().getFullYear()} GoVite Inc. All rights reserved.</p>
+        </div>
+      </footer>
     </div>
   );
 };
