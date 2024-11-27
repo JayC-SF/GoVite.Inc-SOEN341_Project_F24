06d29083fc0edc60cc7ee4297b5617bb782c52f3
Calvin Lee <calvintp4414@gmail.com>
2024-11-26 19:33:43 -0500

    Inside pages footer

--- frontend/src/templates/SidebarPageTemplate.tsx
+++ frontend/src/templates/SidebarPageTemplate.tsx
@@ -1,17 +1,22 @@
 import { ReactNode } from 'react'
 import { Sidebar } from '../modules/Sidebar'
-interface SidebarPageTemplateProps { 
+import Footer from '../modules/Footer'
+interface SidebarPageTemplateProps {
     hidden: boolean,
-    children?: ReactNode 
+    children?: ReactNode
 }
 export default function SidebarPageTemplate({ hidden, children }: SidebarPageTemplateProps) {
-    
+
     return !hidden && (
-        <div className='grid grid-cols-[235px,_1fr] text-stone-950 bg-[#9B394B] h-[100vh] box-border'>
+        <><div className='grid grid-cols-[235px,_1fr] text-stone-950 bg-[#9B394B] h-[100vh] box-border'>
             <Sidebar />
             <div className='bg-white rounded-lg shadow overflow-y-scroll min-h-full'>
                 {children}
             </div>
         </div>
+            <div>
+                <Footer />
+            </div>
+        </>
     )
 }
