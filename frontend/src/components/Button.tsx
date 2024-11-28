6b59fef85cf25924cbfe79c932f12f01de07c63f
Juan-Carlos Sreng-Flores <srengfloresj@hotmail.com>
2024-11-28 14:40:59 -0500

    fix to const

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,7 +1,7 @@
 export function RMPButton(
   props: React.ButtonHTMLAttributes<HTMLButtonElement>
 ) {
-  let classname = props.disabled == true? "bg-gray-500 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded-md transition duration-200" : 
+  const classname = props.disabled == true? "bg-gray-500 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded-md transition duration-200" : 
    "bg-primary-red text-white font-bold py-2 px-4 rounded-md hover:bg-red-600 transition duration-200`"
   return (
     <button
